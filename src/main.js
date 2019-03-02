883967548414a7f56d464572de194db2e441f376
joecosta117 <joecosta117@gmail.com>
2019-03-02 14:21:02 -0500

    added more stuff to the main page

--- src/main.js
+++ src/main.js
@@ -6,6 +6,21 @@ const Main = () => (
       <p>Joe Costa</p>
       <p>Full Stack Developer</p>
     </div>
+    <div>
+      <h1>Technical Skills</h1>
+      <div>
+        <p>Javascript</p>
+        <p>Ruby</p>
+        <p>Node.js</p>
+        <p>React</p>
+        <p>Redux</p>
+        <p>CSS</p>
+        <p>HTML</p>
+        <p>PostgreSQL</p>
+        <p>Sequelize</p>
+        <p>Express</p>
+      </div>
+    </div>
   </div>
 )
 
